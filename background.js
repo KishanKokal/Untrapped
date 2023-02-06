e7d9a279ef52f39171dd36f00b4466fe16fc27c1
Kishan Kokal <kokalkishan.official@gmail.com>
2023-02-07 00:42:36 +0530

    Update content.js and background.js to create a button that makes youtube homepage and sidebar disappear

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log('Hello, this is background script running!')
+console.log('Hello, this is background script running!');
+
+chrome.action.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+    let message = {
+        text: 'hello'
+    }
+    chrome.tabs.sendMessage(tab.id, message)
+}
--- content.js
+++ content.js
@@ -1,22 +1,26 @@
-// Wait for 3 seconds to load the page
-setTimeout(function (){
+chrome.runtime.onMessage.addListener(gotMessage)
 
-    // declaring variables to store each element
-    let videoSideBar = document.getElementById('secondary');
-    let homeFeed = document.querySelector('div#contents.style-scope.ytd-rich-grid-renderer');
+function gotMessage(message, sender, sendResponse) {
+    if (message.text === 'hello') {
+        // declaring variables to store each element
+        let videoSideBar = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
+        let homeFeed = document.querySelector('div#contents.style-scope.ytd-rich-grid-renderer');
 
-    // check if the page has a side bar
-    if (videoSideBar) {
-        // set the visibility of side bar to hidden
-        videoSideBar.style['visibility'] = 'hidden';
-        console.log("Side Bar is now disappeared");
+        // check if the page has a side bar
+        if (videoSideBar) {
+            // set the visibility of side bar to hidden
+            videoSideBar[0].style['visibility'] = 'hidden';
+            console.log("Side Bar is now disappeared");
+            videoSideBar = false;
+        }
+
+        // check if the page has a home page
+        if (homeFeed) {
+            // set the visibility of home page to hidden
+            homeFeed.style['visibility'] = 'hidden';
+            console.log("Home feed is now disappeared")
+            homeFeed = false;
+        }
     }
-
-    // check if the page has a home page
-    if (homeFeed) {
-        // set the visibility of home page to hidden
-        homeFeed.style['visibility'] = 'hidden';
-        console.log("Home feed is now disappeared")
-    }
-
-}, 3000);
+}
+    
