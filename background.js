b76c1915a2cf723a5fc92185b9b45aa0290225ec
Kishan Kokal <kokalkishan.official@gmail.com>
2023-02-12 23:40:28 +0530

    Fix content.js: resolved issue with home page not behaving as intended

--- background.js
+++ background.js
@@ -1,13 +1,11 @@
 console.log('Hello, this is background script running!');
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes('https://www.youtube.com')) {
+    if (tab.url  && tab.url.includes('youtube.com')) {
         console.log('YT video page loaded!');
         callContent(tabId);
     }
 });
-
-// chrome.action.onClicked.addListener(buttonClicked);
 
 function callContent(tabId) {
     console.log("Button Clicked!");
--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-    console.log("got message called");
 
-    if (message.text === 'hello') {
+    console.log("got message called");
+    performAction(message.text);
+
+}
+
+function performAction(message) {
+    console.log("Perform action called!");
+    if (message === 'hello') {
         // declaring variables to store each element
         let videoSideBar = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
         let adsSection = document.getElementById('companion');
@@ -11,43 +17,44 @@ function gotMessage(message, sender, sendResponse) {
         let homeFeedIronSelector = document.querySelector("#chips.ytd-feed-filter-chip-bar-renderer");
         let youtubeLogo = document.querySelector('#logo-icon');
 
-        if (youtubeLogo) {
-            youtubeLogo.innerHTML = '<img src = "https://raw.githubusercontent.com/KishanKokal/Untrapped/a47dc370e9491f3f40aaa81ec8188db166709ee7/untrapped.svg" width = "100px"></img>';
-            console.log("The YouTube logo has been changed");
-            youtubeLogo = false;
-        }
-
-        // check if the page has a side bar
-        if (videoSideBar) {
-            // set the visibility of side bar to hidden
-            videoSideBar[0].style['visibility'] = 'hidden';
-            console.log("Side Bar is now disappeared");
-            videoSideBar = false;
-        }
-
-        // check if the page has a ads sectio
-        if (adsSection) {
-            // set the visibility of the ads section to hidden
-            adsSection.style['visibility'] = 'hidden';
-            console.log("Ads section is now removed");
-            adsSection = false;
-        }
-
         // check if the page has a homefeed
-        if (homeFeed) {
+        if (homeFeed !== undefined) {
             // set the visibility of home page to hidden
-            homeFeed.style['visibility'] = 'hidden';
+            homeFeed.style['display'] = 'none';
             console.log("Home feed is now disappeared")
-            homeFeed = false;
+            homeFeed = undefined;
         }
 
         // check if the page has homeFeedIronSelector
-        if (homeFeedIronSelector) {
+        if (homeFeedIronSelector !== undefined) {
             // set the visibility of home page to hidden
             homeFeedIronSelector.style['visibility'] = 'hidden';
             console.log("Home feed iron selector is now disappeared")
-            homeFeedIronSelector = false;
+            homeFeedIronSelector = undefined;
+        }
+
+        if (youtubeLogo !== undefined) {
+            youtubeLogo.innerHTML = '<img src = "https://raw.githubusercontent.com/KishanKokal/Untrapped/a47dc370e9491f3f40aaa81ec8188db166709ee7/untrapped.svg" width = "100px"></img>';
+            console.log("The YouTube logo has been changed");
+            youtubeLogo = undefined;
+        }
+
+        // check if the page has a side bar
+        if (videoSideBar !== undefined) {
+            // set the visibility of side bar to hidden
+            videoSideBar[0].style['visibility'] = 'hidden';
+            console.log("Side Bar is now disappeared");
+            videoSideBar = undefined;
+        }
+
+        // check if the page has a ads sectio
+        if (adsSection !== undefined) {
+            // set the visibility of the ads section to hidden
+            adsSection.style['visibility'] = 'hidden';
+            console.log("Ads section is now removed");
+            adsSection = undefined;
         }
     }
 }
-    
+
+performAction('hello');
