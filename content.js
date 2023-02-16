8cfeca07bf95b393281105ca251f4fd43686fdb0
Kishan Kokal <kokalkishan.official@gmail.com>
2023-02-16 22:35:39 +0530

    Refactor Content.js, improve performance and fix bugs
    The changes include moving the variable declarations outside of the function and changing undefined checks to null checks for better consistency. Additionally, the code checks for an undefined first element in the video sidebar array to avoid a TypeError, and sets variables to null instead of undefined after they are no longer needed.

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 
+// declaring variables to store each element
+let videoSideBar = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
+let adsSection = document.getElementById('companion');
+let homeFeed = document.getElementById('contents');
+let homeFeedIronSelector = document.querySelector("#chips.ytd-feed-filter-chip-bar-renderer");
+let youtubeLogo = document.querySelector('#logo-icon');
+
 function gotMessage(message, sender, sendResponse) {
 
     console.log("got message called");
@@ -11,48 +18,47 @@ function performAction(message) {
     console.log("Perform action called!");
     if (message === 'hello') {
         // declaring variables to store each element
-        let videoSideBar = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
-        let adsSection = document.getElementById('companion');
-        let homeFeed = document.getElementById('contents');
-        let homeFeedIronSelector = document.querySelector("#chips.ytd-feed-filter-chip-bar-renderer");
-        let youtubeLogo = document.querySelector('#logo-icon');
+        videoSideBar = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
+        adsSection = document.getElementById('companion');
+        homeFeed = document.getElementById('contents');
+        homeFeedIronSelector = document.querySelector("#chips.ytd-feed-filter-chip-bar-renderer");
+        youtubeLogo = document.querySelector('#logo-icon');
 
         // check if the page has a homefeed
-        if (homeFeed !== undefined) {
+        if (homeFeed !== null) {
             // set the visibility of home page to hidden
             homeFeed.style['display'] = 'none';
             console.log("Home feed is now disappeared")
-            homeFeed = undefined;
+            homeFeed = null;
         }
 
         // check if the page has homeFeedIronSelector
-        if (homeFeedIronSelector !== undefined) {
+        if (homeFeedIronSelector !== null) {
             // set the visibility of home page to hidden
             homeFeedIronSelector.style['visibility'] = 'hidden';
             console.log("Home feed iron selector is now disappeared")
-            homeFeedIronSelector = undefined;
+            homeFeedIronSelector = null;
         }
 
-        if (youtubeLogo !== undefined) {
+        if (youtubeLogo !== null) {
             youtubeLogo.innerHTML = '<img src = "https://raw.githubusercontent.com/KishanKokal/Untrapped/a47dc370e9491f3f40aaa81ec8188db166709ee7/untrapped.svg" width = "100px"></img>';
             console.log("The YouTube logo has been changed");
-            youtubeLogo = undefined;
+            youtubeLogo = null;
         }
 
         // check if the page has a side bar
-        if (videoSideBar !== undefined) {
+        if (videoSideBar !== null && videoSideBar[0] !== undefined) {
             // set the visibility of side bar to hidden
             videoSideBar[0].style['visibility'] = 'hidden';
             console.log("Side Bar is now disappeared");
-            videoSideBar = undefined;
         }
 
         // check if the page has a ads sectio
-        if (adsSection !== undefined) {
+        if (adsSection !== null) {
             // set the visibility of the ads section to hidden
             adsSection.style['visibility'] = 'hidden';
             console.log("Ads section is now removed");
-            adsSection = undefined;
+            adsSection = null;
         }
     }
 }
