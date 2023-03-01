9ff5bc4477ea7a23a1688e62bac1ec07a6849eda
Kishan Kokal <kokalkishan.official@gmail.com>
2023-03-01 22:35:00 +0530

    Redesign popup.html for improved user experience
    This commit redesigns the popup.html file to provide a more intuitive and visually appealing user experience. The changes include updated font styles, color scheme, and layout. The goal of this redesign is to make the extension more user-friendly and enhance the overall user experience.

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 console.log('Hello, this is background script running!');
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url && tab.url.includes('youtube.com')) {
         console.log('YT video page loaded!');
